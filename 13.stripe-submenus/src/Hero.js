f88245502bb9a2f93e7855d4e7198aef1e614384
ASO-CODE <60587094+ASO-CODE@users.noreply.github.com>
2022-08-24 02:48:54 +0100

    New Commit

--- 13.stripe-submenus/src/Hero.js
+++ 13.stripe-submenus/src/Hero.js
@@ -1,8 +1,27 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const { closeSubmenu } = useGlobalContext();
 
-export default Hero
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut magnam
+            ipsum, sunt consequatur perferendis ducimus.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
