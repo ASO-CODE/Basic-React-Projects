855b22b153aaffce256393da4ba193b5cc3fff97
ASO-CODE <60587094+ASO-CODE@users.noreply.github.com>
2022-09-18 12:30:32 +0100

    New Commit

--- 15.cocktails/src/pages/About.js
+++ 15.cocktails/src/pages/About.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae minima
+        soluta repellendus et natus! Cumque fuga dolores est dignissimos a Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta provident a maxime quidem nihil nemo eos deleniti laboriosam explicabo aliquid hic fugiat omnis earum optio ea mollitia voluptatum, rem adipisci?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
